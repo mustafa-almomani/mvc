c2f548c068b60f21b3e2e0c6a9d3d5a9ee059a40
mustafa-almomani <almomani363@gmail.com>
2024-07-22 17:50:27 +0300

    task1 mvc 22-7

---
+++ test2/test2/Controllers/loginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test2.Controllers
+{
+    public class loginController : Controller
+    {
+        // GET: login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult about()
+        {
+            return View();
+        }
+        public ActionResult contactus()
+        {
+            return View();
+        }
+        public ActionResult home()
+        {
+            return View();
+        }
+        public ActionResult serves()
+        {
+            return View();
+        }
+    }
+}
